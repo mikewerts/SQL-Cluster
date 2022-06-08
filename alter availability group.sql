8a2f689cfe98d75a026536376d58d3e70b4712aa
mikewerts <47601447+mikewerts@users.noreply.github.com>
2022-06-08 12:45:40 -0400

    Add files via upload

---
+++ alter availability group.sql
@@ -0,0 +1,242 @@
+--- YOU MUST EXECUTE THE FOLLOWING SCRIPT IN SQLCMD MODE.
+:Connect PR-SQLCLSTR-02\IRSQL
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [PR-SQLAG-02]
+ADD DATABASE [ImageRight];
+
+GO
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [PR-SQLAG-02]
+ADD DATABASE [ImageRightInstallerDB];
+
+GO
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [PR-SQLAG-02]
+ADD DATABASE [IRDW];
+
+GO
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [PR-SQLAG-02]
+ADD DATABASE [IRinstallerDB];
+
+GO
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [PR-SQLAG-02]
+ADD DATABASE [TestIntranetConnections];
+
+GO
+
+:Connect PR-SQL-02\IRSQL02
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'PR-SQLAG-02'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [ImageRight] SET HADR AVAILABILITY GROUP = [PR-SQLAG-02];
+
+GO
+
+:Connect PR-SQL-02\IRSQL02
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'PR-SQLAG-02'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [ImageRightInstallerDB] SET HADR AVAILABILITY GROUP = [PR-SQLAG-02];
+
+GO
+
+:Connect PR-SQL-02\IRSQL02
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'PR-SQLAG-02'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [IRDW] SET HADR AVAILABILITY GROUP = [PR-SQLAG-02];
+
+GO
+
+:Connect PR-SQL-02\IRSQL02
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'PR-SQLAG-02'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [IRinstallerDB] SET HADR AVAILABILITY GROUP = [PR-SQLAG-02];
+
+GO
+
+:Connect PR-SQL-02\IRSQL02
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'PR-SQLAG-02'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [TestIntranetConnections] SET HADR AVAILABILITY GROUP = [PR-SQLAG-02];
+
+GO
+
+
+GO
+
+
